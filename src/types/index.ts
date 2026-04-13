1d6d14f5d751a50ea8cb56337754003a67fbc531
janjaw007 <chayanit.kitm@gmail.com>
2026-04-13 14:45:18 +0700

    feate: setup TypeScript interfaces for menu and cart

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface MenuItem {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+}
+
+export interface CartItem extends MenuItem {
+  quantity: number;
+}
