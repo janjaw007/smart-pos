fe6827636483fbc913bf91a397017766e2f5ed6c
janjaw007 <chayanit.kitm@gmail.com>
2026-04-13 15:27:49 +0700

    feat:integrate MenuCard and MockData into App

---
+++ src/components/MenuCard.tsx
@@ -0,0 +1,27 @@
+import type { MenuItem } from "../types";
+
+interface MenuCardProps {
+  menu: MenuItem;
+}
+
+function MenuCard({ menu }: MenuCardProps) {
+  return (
+    <div className="border border-gray-200  p-4 rounded-2xl text-center shadow-md hover:shadow-lg transition-shadow bg-white">
+      <img
+        src={menu.image}
+        alt={menu.name}
+        className="w-full h-48 object-cover rounded-xl"
+      />
+      <h3 className="mt-4 mb-1 text-lg font-bold text-gray-800">{menu.name}</h3>
+      <p className="text-orange-500 font-bold text-xl mb-4">
+        ${menu.price.toFixed(2)}
+      </p>
+
+      <button className="bg-orange-500 hover:bg-amber-600 text-white w-full py-2 rounded-xl font-semibold transition-colors cursor-pointer">
+        เพิ่มลงตะกร้า
+      </button>
+    </div>
+  );
+}
+
+export default MenuCard;
